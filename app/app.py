a1aacb39aad3f69c067ba89dd4008316aa56c937
Surabhi Sahay <surabhi.sahay03@gmail.com>
2025-05-29 06:00:10 -0400

    Committing the files

---
+++ app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host='db',
+        database='testdb',
+        user='admin',
+        password='adminpass'
+    )
+    return conn
+
+@app.route('/')
+def home():
+    return "Flask app connected to PostgreSQL!"
+
+@app.route('/users')
+def users():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users;')
+    users = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
